c5f9cde1a99ca36240a6cadc70bdc758510df86c
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-01-14 20:23:06 -0500

    Backend project foundation

---
+++ Backend/Crunchyroll Clone/src/main/java/dev/juda/JuDaDevApp.java
@@ -0,0 +1,13 @@
+package dev.juda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuDaDevApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuDaDevApp.class, args);
+    }
+
+}
