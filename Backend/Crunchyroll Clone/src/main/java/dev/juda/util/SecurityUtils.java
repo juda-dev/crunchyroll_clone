345a8964fc5c9af42379a0351a38c80bfeb8bb7a
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-01-21 13:06:53 -0500

    Implemented JWT generation, validation, and claims extraction methods in SecurityUtils, enabling username and role retrieval, and annotated the class as a Spring Component.

--- Backend/Crunchyroll Clone/src/main/java/dev/juda/util/SecurityUtils.java
+++ Backend/Crunchyroll Clone/src/main/java/dev/juda/util/SecurityUtils.java
@@ -1,14 +1,76 @@
 package dev.juda.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
+@Component
 public class SecurityUtils {
 
-    public static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+    private static final long JWT_TOKEN_VALIDITY = 15L * 60 * 1000;
+    private static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
     public final static String PREFIX_TOKEN = "Bearer ";
     public final static String HEADER_AUTHORIZATION = "Authorization";
     public static final String CONTENT_TYPE = "application/json";
 
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public String getRoleFromToken(String token) {
+        return getClaimFromToken(token, claims -> claims.get("role", String.class));
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public String generatedToken(String username, String role) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+        return doGenerateToken(username, claims);
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            getAllClaimsFromToken(token);
+            return !isTokenExpiration(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(SECRET_KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private String doGenerateToken(String username, Map<String, Object> claims) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    private Boolean isTokenExpiration(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
 }
