a48c43e29e195e12a5b294f0bc0c01a2b39866bc
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-01-22 22:39:57 -0500

    Implement UserDetailsService

---
+++ Backend/Crunchyroll Clone/src/main/java/dev/juda/security/JpaUserDetailsService.java
@@ -0,0 +1,25 @@
+package dev.juda.security;
+
+import dev.juda.mapper.UserMapper;
+import dev.juda.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    public JpaUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .map(UserMapper::toUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
