64a3a51e0e27cace1b50129aa5e7e77ea0cc19dd
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-01-22 22:36:18 -0500

    Add findByEmail method to UserRepository

--- Backend/Crunchyroll Clone/src/main/java/dev/juda/repository/UserRepository.java
+++ Backend/Crunchyroll Clone/src/main/java/dev/juda/repository/UserRepository.java
@@ -3,7 +3,10 @@ package dev.juda.repository;
 import dev.juda.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByEmail(String email);
 }
