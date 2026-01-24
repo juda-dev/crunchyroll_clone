084a9b9c6ed153f0daa83828d215ae0d4ac9c33b
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-01-23 23:56:03 -0500

    Add Thymeleaf configuration for email templates and update dependencies

---
+++ Backend/Crunchyroll Clone/src/main/java/dev/juda/config/AppConfig.java
@@ -0,0 +1,34 @@
+package dev.juda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    ITemplateResolver iTemplateResolver(){
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/mails");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resolver.setCacheable(true);
+
+        return resolver;
+    }
+
+    @Bean
+    SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(iTemplateResolver());
+
+        return templateEngine;
+    }
+}
