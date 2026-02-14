c6d5b80948dc3c96011532cf777dc845ade90c06
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-02-14 16:36:11 -0500

    Initialize Crunchyroll clone structure and add Landing page
    - Create Landing component with header, hero section, and styling.
    - Add global styles, Lato font, and reset in styles.css.
    - Configure routing to default to the Landing page.
    - Add project assets including logo and background image.

--- Frontend/crunchyroll-clone/src/app/app.routes.ts
+++ Frontend/crunchyroll-clone/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {Landing} from './features/landing/landing';
 
-export const routes: Routes = [];
+export enum FEATURES_PAGES {
+}
+
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: Landing
+      }
+    ]
+  }
+];
