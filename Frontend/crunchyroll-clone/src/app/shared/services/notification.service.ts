3f93d03e039d47fe227c1acf1fcc25e0e37fbd3a
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-02-15 20:27:43 -0500

    Configure Angular Material and implement NotificationService
    Create NotificationService to handle success and error alerts using MatSnackBar.

---
+++ Frontend/crunchyroll-clone/src/app/shared/services/notification.service.ts
@@ -0,0 +1,27 @@
+import {inject, Injectable} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  readonly snackBar = inject(MatSnackBar);
+
+  success(message: string, duration: number = 4000) {
+    this.snackBar.open(message, 'Close', {
+      duration: duration,
+      horizontalPosition: "center",
+      verticalPosition: "top",
+      panelClass: ['notification-success']
+    });
+  }
+
+  error(message: string, duration: number = 5000) {
+    this.snackBar.open(message, 'Close', {
+      duration: duration,
+      horizontalPosition: "center",
+      verticalPosition: "top",
+      panelClass: ['notification-error']
+    });
+  }
+}
