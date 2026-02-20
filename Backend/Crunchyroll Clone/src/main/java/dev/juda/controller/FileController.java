25dc6556fa5028d181ef34cb38033a02410ea522
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-02-20 07:45:57 -0500

    Implement FileController for image operations
    Adds the `FileController` to handle image uploads and serve anime posters and banners.
    
    - Create `FileController` class.
    - Add `uploadImage` endpoint for storing images.
    - Add `serveAnimePoster` and `serveAnimeBanner` endpoints for retrieving resources.

---
+++ Backend/Crunchyroll Clone/src/main/java/dev/juda/controller/FileController.java
@@ -0,0 +1,39 @@
+package dev.juda.controller;
+
+import dev.juda.model.dto.response.MessageResponse;
+import dev.juda.model.dto.response.UploadResponse;
+import dev.juda.service.FileService;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/files")
+public class FileController {
+    private final FileService fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/upload/image")
+    public UploadResponse uploadImage(@RequestParam("file") MultipartFile file) {
+        return fileService.storeImageFile(file);
+    }
+
+    @GetMapping("/images/animes/posters/{uuid}")
+    public ResponseEntity<Resource> serveAnimePoster(
+            @PathVariable String uuid,
+            @RequestHeader HttpHeaders headers) {
+        return fileService.serveImage(uuid);
+    }
+
+    @GetMapping("/images/animes/banners/{uuid}")
+    public ResponseEntity<Resource> serveAnimeBanner(
+            @PathVariable String uuid,
+            @RequestHeader HttpHeaders headers) {
+        return fileService.serveImage(uuid);
+    }
+}
