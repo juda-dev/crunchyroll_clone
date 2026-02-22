373747dbe0cd6242d33f37f57b27bc2ad379da42
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-02-22 16:29:08 -0500

    feat(ui): create anime-item component for the anime list
    - Implement the new anime-item component to encapsulate individual anime display logic
    - Integrate the component into the main animes page to render the list dynamically

---
+++ Frontend/crunchyroll-clone/src/app/features/home/shared/components/anime-item/anime-item.ts
@@ -0,0 +1,18 @@
+import {Component, input} from '@angular/core';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {ImageViewer} from '../../../../../shared/components/image-viewer/image-viewer';
+
+@Component({
+  selector: 'app-anime-item',
+  imports: [
+    MatIconButton,
+    MatIcon,
+    ImageViewer
+  ],
+  templateUrl: './anime-item.html',
+  styleUrl: './anime-item.css',
+})
+export class AnimeItem {
+  readonly anime = input.required<any>();
+}
