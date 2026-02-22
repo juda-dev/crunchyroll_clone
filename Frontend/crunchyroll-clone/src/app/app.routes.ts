aafc86e9cedde2f8656c85c65d5101c093ce5f2c
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-02-21 20:55:17 -0500

    Add home route with lazy loading and auth guard
    Update app.routes.ts to import HOME_PAGES and authGuard. Configure the home path to lazy load the home routes and enforce authentication using authGuard.

--- Frontend/crunchyroll-clone/src/app/app.routes.ts
+++ Frontend/crunchyroll-clone/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import {Routes} from '@angular/router';
 import {Landing} from './features/landing/landing';
 import {AUTH_PAGES} from './features/auth/auth.routes';
+import {HOME_PAGES} from './features/home/home.routes';
+import {authGuard} from './shared/guards/auth-guard';
 
 export const routes: Routes = [
   {
@@ -15,6 +17,12 @@ export const routes: Routes = [
         path: AUTH_PAGES.AUTH,
         loadChildren: () => import('./features/auth/auth.routes')
           .then(r => r.AUTH_ROUTES)
+      },
+      {
+        path: HOME_PAGES.HOME,
+        loadChildren: () => import("./features/home/home.routes")
+          .then(p => p.HOME_ROUTES),
+        canActivate: [authGuard]
       }
     ]
   },
