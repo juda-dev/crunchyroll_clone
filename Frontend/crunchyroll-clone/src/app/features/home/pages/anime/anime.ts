f14b5eb8bd62ff8c2d126a4528d58fff2c1e1c43
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-03-02 13:03:35 -0500

    feat: Implement video management UI for anime page

--- Frontend/crunchyroll-clone/src/app/features/home/pages/anime/anime.ts
+++ Frontend/crunchyroll-clone/src/app/features/home/pages/anime/anime.ts
@@ -1,16 +1,89 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input, OnInit, signal} from '@angular/core';
 import {HeaderAdmin} from '../../admin/components/header-admin/header-admin';
 import {AnimeHeader} from '../../shared/components/anime-header/anime-header';
+import {VideoItem} from '../../shared/components/video-item/video-item';
+import {VideoService} from '../../shared/services/video.service';
+import {LoadingMoreLoaderService} from '../../../../shared/loaders/loading-more/loading-more-loader.service';
+import {MatDialog} from '@angular/material/dialog';
+import {VideoForm} from '../../admin/components/forms/video-form/video-form';
+import {NotificationService} from '../../../../shared/services/notification.service';
 
 @Component({
   selector: 'app-anime',
   imports: [
     HeaderAdmin,
-    AnimeHeader
+    AnimeHeader,
+    VideoItem
   ],
   templateUrl: './anime.html',
   styleUrl: './anime.css',
 })
-export class Anime {
+export class Anime implements OnInit {
+  readonly #videoService = inject(VideoService);
+  readonly #loaderService = inject(LoadingMoreLoaderService);
+  readonly dialog = inject(MatDialog);
+  readonly #notification = inject(NotificationService);
+  animeName = signal<string>('');
+
+  videos: any = signal<any>([]);
+  loadedVideosIds = new Set<string>();
+
+  isLoading = this.#loaderService.isLoading;
+
   id = input.required<string>();
+
+  constructor() {
+    this.#videoService.resetVideosPage();
+  }
+
+  ngOnInit(): void {
+    this.loadVideos();
+  }
+
+  setAnimeName(animeName: string) {
+    this.animeName.set(animeName);
+  }
+
+  loadVideos() {
+    this.#loaderService.show();
+    this.#videoService.resetVideosPage();
+    this.#videoService.getAllVideosAnime(this.id()).subscribe({
+      next: (res) => {
+        this.videos.set(res.content);
+        this.loadedVideosIds.clear();
+        this.updateLoadedVideosIds();
+      },
+      complete: () => this.#loaderService.hide()
+    });
+  }
+
+  updateLoadedVideosIds() {
+    this.videos().forEach((video: { id: string; }) => this.loadedVideosIds.add(video.id))
+  }
+
+  openVideoModal(videoData?: any) {
+    const dialogRef = this.dialog.open(VideoForm, {
+      width: '95vw',
+      maxWidth: '1600px',
+      height: 'auto',
+      maxHeight: '95vh',
+      panelClass: 'custom-dialog-container',
+      disableClose: true,
+      autoFocus: false,
+      data: {
+        animeId: this.id(),
+        ...(videoData ? { video: videoData } : {})
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.loadVideos();
+      }
+    });
+  }
+
+  editVideo(video: any) {
+    this.openVideoModal(video);
+  }
 }
