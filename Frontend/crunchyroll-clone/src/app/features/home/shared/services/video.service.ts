3b7d41a644864f9e71d99308985ee2cb0cd23af0
Juan David Arboleda Vallecilla <judadev@proton.me>
2026-03-02 12:57:50 -0500

    feat: Add abstract VideoService for video management
    -Created abstract VideoServiceAbstract class defining video CRUD operations

---
+++ Frontend/crunchyroll-clone/src/app/features/home/shared/services/video.service.ts
@@ -0,0 +1,45 @@
+import {inject, Injectable, signal} from '@angular/core';
+import {VideoServiceAbstract} from './video.service.abstract';
+import {delay, EMPTY, finalize, Observable, tap} from "rxjs";
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VideoService extends VideoServiceAbstract {
+  readonly #httpClient = inject(HttpClient);
+  private videosPage = signal(0);
+  public loading = signal(false);
+
+    override getAllVideosAnime(animeUuid: string): Observable<any> {
+      if (this.loading()) return EMPTY;
+
+      this.loading.set(true);
+
+      return this.#httpClient.get<any>(`${this.API_ENDPOINT}/${animeUuid}?page=${this.videosPage()}&size=8`).pipe(
+        delay(350),
+        tap(() => {
+          this.videosPage.update(page => page + 1);
+        }),
+        finalize(() => {
+          this.loading.set(false);
+        })
+      )
+    }
+
+  override resetVideosPage() {
+    this.videosPage.set(0);
+  }
+
+  override updateVideoMetadata(videoUuid: string, videoData: any): Observable<any> {
+    return this.#httpClient.put(`${this.API_ENDPOINT}/${videoUuid}`, videoData);
+  }
+
+  override createVideoMetadata(videoData: any): Observable<any> {
+    return this.#httpClient.post(`${this.API_ENDPOINT}`, videoData);
+  }
+
+  override deleteVideoMetadata(videoUuid: string): Observable<any> {
+    return this.#httpClient.delete(`${this.API_ENDPOINT}/${videoUuid}`);
+  }
+}
